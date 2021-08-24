4f98a03fa1d9c13423a64c3d7d0ff32b480d0ef3
Ahmed-Sabry-Ali <89470818+Ahmed-Sabry-Ali@users.noreply.github.com>
2021-08-24 17:46:15 +0200

    Add files via upload

---
+++ ECO_Master/Debug/DIO_prog.d
@@ -0,0 +1,10 @@
+DIO_prog.o DIO_prog.o: ../DIO_prog.c ../lib/types.h ../lib/BIT_math.h \
+  ../DIO_interface.h ../DIO_prv.h
+
+../lib/types.h:
+
+../lib/BIT_math.h:
+
+../DIO_interface.h:
+
+../DIO_prv.h:
---
+++ ECO_Master/Debug/LCD_prog.d
@@ -0,0 +1,12 @@
+LCD_prog.o LCD_prog.o: ../LCD_prog.c ../lib/types.h ../DIO_interface.h \
+  ../LCD_interface.h ../LCD_config.h ../LCD_prv.h
+
+../lib/types.h:
+
+../DIO_interface.h:
+
+../LCD_interface.h:
+
+../LCD_config.h:
+
+../LCD_prv.h:
