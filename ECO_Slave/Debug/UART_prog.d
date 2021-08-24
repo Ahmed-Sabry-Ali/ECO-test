146049fcfd359e13208955b2e6600dcc095279a0
Ahmed-Sabry-Ali <89470818+Ahmed-Sabry-Ali@users.noreply.github.com>
2021-08-24 17:46:59 +0200

    Add files via upload

---
+++ ECO_Slave/Debug/UART_prog.d
@@ -0,0 +1,10 @@
+UART_prog.o UART_prog.o: ../UART_prog.c ../lib/types.h ../lib/BIT_math.h \
+  ../UART_interface.h ../UART_prv.h
+
+../lib/types.h:
+
+../lib/BIT_math.h:
+
+../UART_interface.h:
+
+../UART_prv.h:
---
+++ ECO_Slave/Debug/main.d
@@ -0,0 +1,12 @@
+main.o main.o: ../main.c ../lib/types.h ../lib/BIT_math.h \
+  ../DIO_interface.h ../LCD_interface.h ../UART_interface.h
+
+../lib/types.h:
+
+../lib/BIT_math.h:
+
+../DIO_interface.h:
+
+../LCD_interface.h:
+
+../UART_interface.h:
